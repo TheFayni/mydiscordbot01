69114e1593a6b58b8218b0cb7d403180eade034c
TheFayni <78732148+TheFayni@users.noreply.github.com>
2021-02-09 16:10:06 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os 
+
 settings = {
-    'TOKEN': 'NzkyMTIxNzA4NjM0NDM5Njkx.X-ZG1A.FJDyllxqUX5vp2FbuG5H3irP1jg',
+    'TOKEN': '',
     'NAME BOT': 'TurboBot',
     'ID': 792121708634439691,
     'PREFIX': '$'
 }
+
+settings['TOKEN'] = os.environ.get('BOT_TOKEN')
